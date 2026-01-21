759816cbfcce3e5f1e4fd8a8b7590c7b8adee927
João Carvalho <joao.antonio.carvalho27@gmail.com>
2026-01-21 13:47:48 -0600

    Update Code

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendTelegram: (message, pnl) => ipcRenderer.invoke('send-telegram', { message, pnl }),
-    closeApp: () => ipcRenderer.send('close-app') // Envia um evento para o main process
-});
